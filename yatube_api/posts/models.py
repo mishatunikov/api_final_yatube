3628876e842585d3605cc2ff277e6ecd17ead3d7
mishatunikov <michael.tunikov@gmail.com>
2025-02-14 17:28:21 +0300

    Добавлена модель Follow.

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -43,3 +43,12 @@ class Comment(models.Model):
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_following'
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE,  related_name='followers'
+    )
