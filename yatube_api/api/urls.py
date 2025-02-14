de5cb14371f4c53dc6f18059bb11a1a133d239b3
mishatunikov <michael.tunikov@gmail.com>
2025-02-14 12:32:18 +0300

    Добавлены ендпоинты для работы с моделями Post и Group.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,17 +1,28 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+from api.views import PostViewSet, CommentViewSet, GroupViewSet
+
+router_v1 = DefaultRouter()
+
+router_v1.register('posts', PostViewSet)
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment'
+)
+router_v1.register('groups', GroupViewSet)
 
 jwt_endpoints = [
     path('create/', TokenObtainPairView.as_view(), name='token_create_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('verify/', TokenVerifyView.as_view(), name='token_verify')
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 urlpatterns = [
+    path('v1/', include(router_v1.urls)),
     path('v1/jwt/', include(jwt_endpoints)),
 ]
