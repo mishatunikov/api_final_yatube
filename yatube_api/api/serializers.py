5166852e130bcc5b4f8b83361743b6b269a9ff22
mishatunikov <michael.tunikov@gmail.com>
2025-02-15 15:06:46 +0300

    Добавлена валидация в сериализаторе модели Follow.

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
 from posts.models import Comment, Follow, Group, Post, User
 
@@ -41,8 +42,33 @@ class FollowSerializer(serializers.ModelSerializer):
     following = serializers.SlugRelatedField(
         slug_field='username', queryset=User.objects.all()
     )
-    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+    )
 
     class Meta:
         model = Follow
-        fields = ('user', 'following',)
+        fields = (
+            'user',
+            'following',
+        )
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=(
+                    'user',
+                    'following',
+                ),
+            )
+        ]
+
+    def validate_following(self, value):
+        if value == self.context.get('request').user:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться сами на себя.'
+            )
+        return value
+
+
